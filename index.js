73e798c06646530248ef0cfa0c3d912014cec95c
antoniokallo <antoniokallo@gmail.com>
2022-07-19 14:34:22 -0400

    commit tree 780425baaad9544f851516dbda22980472d386b8

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name) 
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function removeLastCat(){
+    cats.pop()
+}
+function appendCat(name){
+
+    const newcats = [...cats, name];
+    return (newcats)
+
+}
+function prependCat(name){
+    const newcats = [name, ...cats];
+    return (newcats)
+
+}
+function removeLastCat() {
+    const newcats = [...cats];
+    newcats.splice(2,1)
+
+    return (newcats)
+}
+function removeFirstCat() {
+    const newcats = [...cats];
+    newcats.splice(0,1)
+
+    return (newcats)
+}
